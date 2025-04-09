4136f670a9a4195ba08d33b353382d1ed40d2adb
asim <asimneupane11@gmail.com>
2025-04-09 12:06:12 +0545

    Add new TypeScript segments and a video file for course content
    - Added segments 296 to 355 as new TypeScript files in the course directory.
    - Included a new video file for enhanced learning materials.

--- index.js
+++ index.js
@@ -1,54 +1,95 @@
-import express from "express";
-import cors from "cors";
-import multer  from "multer";
-import {v4 as uuidv4} from "uuid"
+
+import express from "express"
+import cors from "cors"
+import multer from "multer"
+import { v4 as uuidv4 } from "uuid"
 import path from "path"
+import fs from "fs"
+import {exec} from "child_process" // watch out
+import { stderr, stdout } from "process"
 
+const app = express()
 
-const app = express();
+//multer middleware
 
-// multer middleware
 const storage = multer.diskStorage({
-    // cb is callback
-    destination:function(req,res,cb){
-        cb(null, "./uploads")
-
-    },
-    filename:function(req,file,cb){
+  destination: function(req, file, cb){
+    cb(null, "./uploads")
+  },
+  filename: function(req, file, cb){
         // path.extname allow extension to be at the end of the filename
-        cb(null, file.fieldname + "-" + uuidv4() + path.extname(file.originalname))
-    }
+    cb(null, file.fieldname + "-" + uuidv4() + path.extname(file.originalname))
+  }
 })
-// multer config
-const upload = multer({
-    storage:storage
-})
-app.use(cors({
-    origin:[
-        "http://localhost:3000",
-        "http://localhost:5173",
-        
-    ],
-    credentials:true
-}))
 
-app.use((req,res,next)=>{
-    res.header("Access-Control-Allow-Origin",)
-    res.header("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    )
+// multer configuration
+const upload = multer({storage: storage})
 
-next();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5173"],
+    credentials: true
+  })
+)
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*") // watch it
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next()
 })
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use("/uploads", express.static("uploads"))
-app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-app.get("/",(req,res)=>{
-    res.json({message:"Hello World"})
+
+app.get('/', function(req, res){
+  res.json({message: "Hello chai aur code"})
 })
-app.post("/upload",upload.single("file"),(req,res)=>{
-    console.log("File uploaded")
+
+app.post("/upload", upload.single('file'), function(req, res){
+  const lessonId = uuidv4()
+  const videoPath = req.file.path
+  const outputPath = `./uploads/courses/${lessonId}`
+   //hls stands for http live streaming developed by apple for delivering live and on-demand streaming content over http.
+
+  const hlsPath = `${outputPath}/index.m3u8`
+  // m3u8 is a  UTF-8 encoded playlist file
+//  plain text file that used to store the url paths of audio,video and information about the media tracks
+//  using this we are able to go forward backward in video its like a index of the information 
+  console.log("hlsPath", hlsPath)
+
+  if (!fs.existsSync(outputPath)) {
+    fs.mkdirSync(outputPath, {recursive: true})
+  }
+
+  // ffmpeg
+  const ffmpegCommand = `ffmpeg -i ${videoPath} -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/segment%03d.ts" -start_number 0 ${hlsPath}
+
+`;
+
+  // no queue because of POC, not to be used in production
+  exec(ffmpegCommand, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`exec error: ${error}`)
+    }
+    console.log(`stdout: ${stdout}`)
+    console.log(`stderr: ${stderr}`)
+    const videoUrl = `http://localhost:8080/uploads/courses/${lessonId}/index.m3u8`;
+
+    res.json({
+      message: "Video converted to HLS format",
+      videoUrl: videoUrl,
+      lessonId: lessonId
+    })
+  })
+//acc is a codex a packager which merge our video , audio , subtitle
+
 })
-app.listen(8080,()=>{
-    console.log("server running")
+
+app.listen(8080, function(){
+  console.log("App is listening at port 8080...")
 })
